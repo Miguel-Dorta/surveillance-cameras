81f5dfc24176490dcbabe459d3e1aa1a8d4b6ce1
Miguel-Dorta <contact@migueldorta.com>
2019-03-28 17:31:49 +0000

    Improved listLargeDirs.go

--- listLargeDirs.go
+++ listLargeDirs.go
@@ -1,34 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
 func main() {
-	f, err := os.Open(os.Args[1])
+	if len(os.Args) > 2 {
+		fmt.Println("Usage:    ./listLargeDirs [path-optional]")
+		os.Exit(1)
+	}
+
+	var path string
+	if len(os.Args) == 2 {
+		path = os.Args[1]
+	} else {
+		path, err := os.Getwd()
+		if err != nil {
+			fmt.Printf("Error getting working directory: %s\nTry using ./listLargeDirs <absolutePath>\n", err.Error())
+			os.Exit(1)
+		}
+	}
+
+	f, err := os.Open(path)
 	if err != nil {
-		println("Error: 1\n" + err.Error())
+		fmt.Printf("Error opening directory: %s\n", err.Error())
+		os.Exit(1)
 	}
 	defer f.Close()
 
+	errCounter := 0
 	for {
 		list, err := f.Readdir(1000)
 		if err != nil {
 			if err.Error() == "EOF" {
-				break;
+				break
 			}
-			println("Error: 2\n" + err.Error())
+			errCounter++
+			fmt.Printf("Error listing files [try %d of 10]: %s\n", errCounter, err.Error())
+
+			if errCounter >= 10 {
+				break
+			}
 		}
 
 		for _, fi := range list {
-			println(fi.Name() + " @ IsDir? " + btoa(fi.IsDir()))
+			fmt.Printf("%s @ IsDir? %t\n", fi.Name(),fi.IsDir())
 		}
 	}
-}
-
-func btoa(b bool) string {
-	if b {
-		return "true"
-	}
-	return "false"
+	os.Exit(errCounter)
 }
