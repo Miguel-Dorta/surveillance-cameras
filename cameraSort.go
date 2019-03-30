e26e0c9ded4f995a3d9e204ab4488274449c6436
Miguel-Dorta <accounts@migueldorta.com>
2019-03-30 22:57:14 +0000

    Improved cameraSort.go

--- cameraSort.go
+++ cameraSort.go
@@ -1,74 +1,121 @@
 package main
 
 import (
-	"os"
 	"fmt"
-	"regexp"
-	"strings"
-	"io/ioutil"
+	"io"
+	"os"
+	"path/filepath"
 )
 
-const (
-	PS string = string(os.PathSeparator)
-)
-
-var processedFiles uint = 0
+var ErrIncorrectFormat = fmt.Errorf("incorrect format")
 
 func main() {
+	if len(os.Args) == 3 {
+		fmt.Println("Usage:    ./cameraSort <origin> <destiny>")
+		os.Exit(1)
+	}
+
 	origin := os.Args[1]
 	destiny := os.Args[2]
-	re, err := regexp.Compile("([a-zA-z]+)(\\d{4})(\\d{2})(\\d{2}).*")
 
 	f, err := os.Open(origin)
-	checkError(err, 1)
+	if err != nil {
+		fmt.Printf("Error opening origin: %s\n", err.Error())
+		os.Exit(1)
+	}
 	defer f.Close()
 
-	for ;; {
+	errCounter := 0
+	for {
 		list, err := f.Readdir(1000)
 		if err != nil {
 			if err.Error() == "EOF" {
-				fmt.Printf("\nFiles processed: %v\n", processedFiles)
-				break;
+				break
 			}
-			checkError(err, 2)
-		}
-		listLen := len(list)
-		for index, fi := range list {
-			fm := fi.Mode()
-			name := fi.Name()
-			if fm.IsRegular() && re.MatchString(name) {
-				result := re.FindStringSubmatch(name)
-				path := origin + PS + name
-				suffix := result[1]
-				y := result[2]
-				m := result[3]
-				d := result[4]
-				finalPath := destiny + PS + suffix + PS + y + PS + m + PS + d + PS + name
 
-				err = os.Rename(path, finalPath)
-				if err != nil {
-					if strings.HasSuffix(err.Error(), "invalid cross-device link") {
-						file, err := ioutil.ReadFile(path)
-						checkError(err, 4)
-						err = ioutil.WriteFile(finalPath, file, os.FileMode(0777))
-						checkError(err, 5)
-						err = os.Remove(path)
-						checkError(err, 6)
-					} else {
-						checkError(err, 3)
-					}
-				}
-				processedFiles++
+			if errCounter < 10 {
+				fmt.Printf("[try %d of 10] Error reading origin: %s\n", errCounter, err.Error())
+				errCounter++
+				continue
+			} else {
+				fmt.Println("[SEVERE] Error accumulation")
+				os.Exit(2)
 			}
-			fmt.Printf("\r%v of %v", index + 1, listLen)
 		}
-		print("\n")
+
+		for _, e := range list {
+			eName := e.Name()
+			if !e.Mode().IsRegular() {
+				continue
+			}
+
+			camId, y, m, d, err := getFileNameParts(eName)
+			if err != nil {
+				continue
+			}
+
+			originPath := filepath.Join(origin, eName)
+			destinyPath := filepath.Join(destiny, camId, y, m, d, eName)
+
+			err = os.Rename(originPath, destinyPath)
+			if err != nil {
+				if _, ok := err.(*os.LinkError); !ok {
+					fmt.Printf("Error moving %s to %s: %s\n", originPath, destinyPath, err.Error())
+					continue
+				}
+
+				err = copyFileBetweenDisks(originPath, destinyPath)
+				if err != nil {
+					fmt.Printf("Error copying %s to %s: %s\n", originPath, destinyPath, err.Error())
+					continue
+				}
+			}
+		}
 	}
 }
 
-func checkError(err error, debugNumber int) {
-	if err != nil {
-		fmt.Printf("\nFiles processed: %v\nError %v\n%v\n", processedFiles, debugNumber, err)
-		os.Exit(1)
+// Gets CamID, Year, Month and Day from a string like CAMIDyyyyMMdd
+func getFileNameParts(s string) (camId, y, m, d string, err error) {
+	name := []byte(s)
+	err = ErrIncorrectFormat
+
+	var c byte
+	var camIdSize int
+	for camIdSize, c = range name {
+		if c > 47 && c < 58 { // Is it a number?
+			break
+		}
+		if c < 65 || c > 122 || (c > 90 && c < 97) { // Is it NOT a letter? [a-zA-Z]
+			return
+		}
 	}
+
+	// Check if there's CamID and a full date
+	if camIdSize == 0 || len(name) < camIdSize + 8 {
+		return
+	}
+
+	for i := camIdSize; i < camIdSize + 8; i++ {
+		if name[i] < 48 || name[i] > 57 { // Is it NOT a number?
+			return
+		}
+	}
+	return s[:camIdSize], s[camIdSize:camIdSize+4], s[camIdSize+4:camIdSize+6], s[camIdSize+6:camIdSize+8], nil
+}
+
+func copyFileBetweenDisks(origin, destiny string) (err error) {
+	originFile, err := os.Open(origin)
+	if err != nil {
+		return
+	}
+	defer originFile.Close()
+
+	destinyFile, err := os.Create(destiny)
+	if err != nil {
+		return
+	}
+	defer destinyFile.Close()
+
+	_, err = io.CopyBuffer(destinyFile, originFile, make([]byte, 100*1024))
+	return
 }
