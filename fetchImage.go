95c21cf4671ea76d6dfd6ee1471ccb14471c2135
Miguel-Dorta <contact@migueldorta.com>
2019-04-04 12:17:40 +0100

    Performance improvement in fetchImage.go

--- fetchImage.go
+++ fetchImage.go
@@ -9,8 +9,9 @@ import (
 	"time"
 )
 
+var camName, user, pass, url, path string
+
 func main() {
-	var camName, user, pass, url, path string
 	if len(os.Args) == 6 {
 		camName = os.Args[1]
 		user = os.Args[2]
@@ -19,42 +20,62 @@ func main() {
 		path = os.Args[5]
 	} else if len(os.Args) == 4 {
 		camName = os.Args[1]
-		user = ""
-		pass = ""
 		url = os.Args[2]
 		path = os.Args[3]
+		user = ""
+		pass = ""
 	} else {
 		fmt.Println("Usage:    <camera-name> <user> <password> <url> <path-destiny>\n  [user] and [password] are optional")
 		os.Exit(1)
 	}
 
-	seconds := time.Tick(time.Second)
+	var (
+		dirPath string
+		tomorrow int64
+		now time.Time
+		err error
+		fileExt = filepath.Ext(url)
+		seconds = time.Tick(time.Second)
+	)
 	for {
 		<- seconds
-		date := time.Now()
-		err := getImage(url, user, pass, camName, path, date)
+		now = time.Now().UTC()
+
+		// If date changes, update dirPath
+		if now.Unix() >= tomorrow {
+			tomorrow = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC).Add(time.Hour * 24).UTC().Unix()
+			dirPath = filepath.Join(
+				path,
+				camName,
+				fmt.Sprintf("%04d", now.Year()),
+				fmt.Sprintf("%02d", now.Month()),
+				fmt.Sprintf("%02d", now.Day()),
+			)
+		}
+
+		err = getImage(dirPath, fileExt, now)
 		if err != nil {
 			fmt.Printf("[%04d-%02d-%02d %02d:%02d:%02d] %s\n",
-				date.Year(),
-				date.Month(),
-				date.Day(),
-				date.Hour(),
-				date.Minute(),
-				date.Second(),
+				now.Year(),
+				now.Month(),
+				now.Day(),
+				now.Hour(),
+				now.Minute(),
+				now.Second(),
 				err.Error(),
 			)
 		}
 	}
 }
 
-func getImage(url, user, pass, camName, path string, date time.Time) error {
+func getImage(dirPath, fileExt string, now time.Time) error {
 	// Request image
 	client := &http.Client{ Timeout: time.Second}
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return fmt.Errorf("error creating request: %s", err.Error())
 	}
-	if user != "" {
+	if user != "" || pass != "" {
 		req.SetBasicAuth(user, pass)
 	}
 	resp, err := client.Do(req)
@@ -64,21 +85,14 @@ func getImage(url, user, pass, camName, path string, date time.Time) error {
 	defer resp.Body.Close()
 
 	// Create and open file
-	dirPath := filepath.Join(
-		path,
-		camName,
-		fmt.Sprintf("%04d", date.Year()),
-		fmt.Sprintf("%02d", date.Month()),
-		fmt.Sprintf("%02d", date.Day()),
-	)
-	fPath := filepath.Join(dirPath, fmt.Sprintf("%02d-%02d-%02d%s", date.Hour(), date.Minute(), date.Second(), filepath.Ext(url)))
+	fPath := filepath.Join(dirPath, fmt.Sprintf("%02d-%02d-%02d%s", now.Hour(), now.Minute(), now.Second(), fileExt))
 	f, err := os.Create(fPath)
 	if err != nil {
 		if !os.IsNotExist(err) {
 			return fmt.Errorf("error creating local file: %s", err.Error())
 		}
 
-		// If it fails because there is not container folder, creates it
+		// If it fails because there is not container folders, creates them
 		err := os.MkdirAll(dirPath, 0700)
 		if err != nil {
 			return fmt.Errorf("error creating container folder: %s", err.Error())
