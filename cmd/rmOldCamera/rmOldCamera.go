56399861eda819a7be30450f1224363870d23ca7
Miguel-Dorta <contact@migueldorta.com>
2019-04-21 20:55:57 +0100

    Modified behaviour: rmOldCamera removes old empty folders instead of their content
    It will remove folders older than the ones wanted to preserve, avoiding thousands of empty folders and useless checks.
    
    This requires changes in sortCamera, checking if there exists a folder before coping the file.

--- cmd/rmOldCamera/rmOldCamera.go
+++ cmd/rmOldCamera/rmOldCamera.go
@@ -24,70 +24,52 @@ func main() {
 		fmt.Printf("Error parsing number %s: %s\nIs it really a number?\n", os.Args[2], err.Error())
 		os.Exit(1)
 	}
-	dateToRm := time.Now().AddDate(0, 0, daysToPreserve * -1)
+	dateToRm := time.Now().UTC().AddDate(0, 0, daysToPreserve * -1)
 
-	camList, err := listDir(path)
-	if err != nil {
-		fmt.Printf("Error reading directory \"%s\": %s\n", path, err.Error())
-		os.Exit(1)
-	}
-
-	for _, cam := range camList {
+	err = internal.ForEachInDirectory(path, func(cam os.FileInfo) error {
 		if !cam.IsDir() {
-			continue
+			return nil
 		}
 
-		camPath := filepath.Join(path, cam.Name())
-		yearToRm := dateToRm.Year() // Save in var to avoid unnecessary function calls
-		for y := 1970; y <= yearToRm; y++ {
-			camYearPath := filepath.Join(camPath, strconv.Itoa(y))
+		pathCam := filepath.Join(path, cam.Name())
+		err = internal.ForEachInDirectory(pathCam, func(year os.FileInfo) error {
+			y, err := strconv.Atoi(year.Name())
+			if err != nil {
+				return nil
+			}
+			pathCamYear := filepath.Join(pathCam, year.Name())
 
-			var monthToRm int
-			if y == yearToRm {
-				monthToRm = int(dateToRm.Month())
-			} else {
-				monthToRm = 12
+			if y < dateToRm.Year() {
+				return os.RemoveAll(pathCamYear)
 			}
 
-			for m := 1; m <= monthToRm; m++ {
-				camYearMonthPath := filepath.Join(camYearPath, strconv.Itoa(m))
+			err = internal.ForEachInDirectory(pathCamYear, func(month os.FileInfo) error {
+				m, err := strconv.Atoi(month.Name())
+				if err != nil {
+					return nil
+				}
+				pathCamYearMonth := filepath.Join(pathCamYear, month.Name())
 
-				var dayToRm int
-				if y == yearToRm && m == monthToRm {
-					dayToRm = dateToRm.Day()
-				} else {
-					dayToRm = 32 //31+1
+				if m < int(dateToRm.Month()) {
+					return os.RemoveAll(pathCamYearMonth)
 				}
 
-				for d := 1; d < dayToRm; d++ {
-					camYearMonthDayPath := filepath.Join(camYearMonthPath, strconv.Itoa(d))
-
-					err = internal.ForEachInDirectory(camYearMonthDayPath, func(fi os.FileInfo) error {
-						err = os.Remove(filepath.Join(camYearMonthDayPath, fi.Name()))
-						if err != nil && !os.IsNotExist(err) {
-							return fmt.Errorf("Error removing file \"%s\": %s\n", filepath.Join(camYearMonthDayPath, fi.Name()), err.Error())
-						}
-						return nil
-					})
+				err = internal.ForEachInDirectory(pathCamYearMonth, func(day os.FileInfo) error {
+					d, err := strconv.Atoi(day.Name())
 					if err != nil {
-						if os.IsNotExist(err) {
-							continue
-						}
-						fmt.Printf(":: Errors found removing content of \"%s\"\n%s\n:: Skipping directory...\n", camYearMonthDayPath, err.Error())
+						return nil
 					}
-				}
-			}
-		}
-	}
-}
+					pathCamYearMonthDay := filepath.Join(pathCamYearMonth, day.Name())
 
-func listDir(path string) (list []os.FileInfo, err error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return
-	}
-	defer f.Close()
-
-	list, err = f.Readdir(-1)
-	return
+					if d < dateToRm.Day() {
+						return os.RemoveAll(pathCamYearMonthDay)
+					}
+					return nil
+				})
+				return err
+			})
+			return err
+		})
+		return err
+	})
 }
