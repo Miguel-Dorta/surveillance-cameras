cf358184dc558fb7b7beef9be940fa209202db11
Miguel-Dorta <contact@migueldorta.com>
2019-09-03 15:14:30 +0100

    Replace call to mv command for custom code

--- pkg/utils/files.go
+++ pkg/utils/files.go
@@ -2,21 +2,70 @@ package utils
 
 import (
 	"fmt"
+	"golang.org/x/sys/unix"
+	"io"
 	"os"
-	"os/exec"
 	"path/filepath"
 )
 
+var copyBuffer = make([]byte, 128 * 1024)
+
 // Executes "mv" command creating the necessary directories in destiny
 func Move(origin, destiny string) error {
+	// Create parent directories if they don't exists
 	if err := os.MkdirAll(filepath.Dir(destiny), 0755); err != nil {
 		return fmt.Errorf("cannot create parent directories for path \"%s\": %s", destiny, err)
 	}
 
-	cmd := exec.Command("mv", origin, destiny)
-	if err := cmd.Run(); err != nil {
-		out, _ := cmd.CombinedOutput()
-		return fmt.Errorf("error moving file from \"%s\" to \"%s\": %s. Output: %s", origin, destiny, err, string(out))
+	// Try to move the file by renaming it
+	err := os.Rename(origin, destiny)
+	if err == nil {
+		return nil // Job done
 	}
+
+	// If it failed, and the error was not an invalid cross-device link, return original error
+	if le, _ := err.(*os.LinkError); le.Err != unix.EXDEV {
+		return err
+	}
+
+	// Copy file
+	if err = Copy(origin, destiny); err != nil {
+		return err
+	}
+
+	// Remove origin file after copying
+	if err = os.Remove(origin); err != nil {
+		return fmt.Errorf("error removing origin file in \"%s\": %s", origin, err)
+	}
+
+	return nil
+}
+
+// Copy will copy a file from the first path provided to the last path provided
+func Copy(origin, destiny string) error {
+	// Open origin file to read
+	fOrigin, err := os.Open(origin)
+	if err != nil {
+		return fmt.Errorf("error opening origin file \"%s\": %s", origin, err)
+	}
+	defer fOrigin.Close()
+
+	// Create and open destiny file to write
+	fDestiny, err := os.Create(destiny)
+	if err != nil {
+		return fmt.Errorf("error creating destiny file \"%s\": %s", destiny, err)
+	}
+	defer fDestiny.Close()
+
+	// Copy
+	if _, err = io.CopyBuffer(fDestiny, fOrigin, copyBuffer); err != nil {
+		return fmt.Errorf("error while copying file from \"%s\" to \"%s\": %s", origin, destiny, err)
+	}
+
+	// Close destiny checking for errors
+	if err = fDestiny.Close(); err != nil {
+		return fmt.Errorf("error closing destiny file in \"%s\": %s", destiny, err)
+	}
+
 	return nil
 }
