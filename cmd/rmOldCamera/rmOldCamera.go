41ae5bb8e4b74c84ddbe1de033aef69c3bd8d1e6
Miguel-Dorta <contact@migueldorta.com>
2019-09-03 16:01:10 +0100

    Refactor rmOldCamera.go

--- cmd/rmOldCamera/rmOldCamera.go
+++ cmd/rmOldCamera/rmOldCamera.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/Miguel-Dorta/surveillance-cameras/internal"
+	"github.com/Miguel-Dorta/surveillance-cameras/pkg/utils"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -10,6 +11,17 @@ import (
 )
 
 const USAGE = "<path> <days-to-preserve>"
+
+var earliestTimeToPreserve time.Time
+
+func parseDaysToPreserve(dtp string) time.Time {
+	daysToPreserve, err := strconv.Atoi(dtp)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Error parsing number %s: %s\nIs it really a number?\n", dtp, err)
+		os.Exit(1)
+	}
+	earliestTimeToPreserve = time.Now().AddDate(0, 0, daysToPreserve * -1)
+}
 
 func main() {
 	internal.CheckSpecialArgs(os.Args, USAGE)
@@ -19,57 +31,81 @@ func main() {
 	}
 
 	path := os.Args[1]
-	daysToPreserve, err := strconv.Atoi(os.Args[2])
-	if err != nil {
-		fmt.Printf("Error parsing number %s: %s\nIs it really a number?\n", os.Args[2], err.Error())
+	parseDaysToPreserve(os.Args[2])
+
+	errs := iterateCams(path)
+	if len(errs) != 0 {
+		for _, err := range errs {
+			_, _ = fmt.Fprintln(os.Stderr, err)
+		}
 		os.Exit(1)
 	}
-	dateToRm := time.Now().UTC().AddDate(0, 0, daysToPreserve * -1)
+}
 
-	err = internal.ForEachInDirectory(path, func(cam os.FileInfo) error {
-		if !cam.IsDir() {
+func iterateCams(path string) []error {
+	var errs []error
+	errs2 := utils.ForEachInDirectory(path, func(camDir os.FileInfo) error {
+		// Omit if is not a directory
+		if !camDir.IsDir() {
 			return nil
 		}
 
-		pathCam := filepath.Join(path, cam.Name())
-		err = internal.ForEachInDirectory(pathCam, func(year os.FileInfo) error {
-			y, err := strconv.Atoi(year.Name())
-			if err != nil {
-				return nil
-			}
-			pathCamYear := filepath.Join(pathCam, year.Name())
+		errs = append(errs, iterateYears(filepath.Join(path, camDir.Name()))...)
+		return nil
+	})
 
-			if y < dateToRm.Year() {
-				return os.RemoveAll(pathCamYear)
-			}
+	return append(errs, errs2...)
+}
 
-			err = internal.ForEachInDirectory(pathCamYear, func(month os.FileInfo) error {
-				m, err := strconv.Atoi(month.Name())
-				if err != nil {
-					return nil
-				}
-				pathCamYearMonth := filepath.Join(pathCamYear, month.Name())
+func iterateYears(path string) []error {
+	var errs []error
+	errs2 := utils.ForEachInDirectory(path, func(yearDir os.FileInfo) error {
+		yearInt, err := strconv.Atoi(yearDir.Name())
+		if err != nil {
+			return fmt.Errorf("error parsing year in \"%s\": %s", path, err)
+		}
 
-				if m < int(dateToRm.Month()) {
-					return os.RemoveAll(pathCamYearMonth)
-				}
+		if yearInt < earliestTimeToPreserve.Year() {
+			return os.RemoveAll(filepath.Join(path, yearDir.Name()))
+		}
 
-				err = internal.ForEachInDirectory(pathCamYearMonth, func(day os.FileInfo) error {
-					d, err := strconv.Atoi(day.Name())
-					if err != nil {
-						return nil
-					}
-					pathCamYearMonthDay := filepath.Join(pathCamYearMonth, day.Name())
+		errs = append(errs, iterateMonths(filepath.Join(path, yearDir.Name()))...)
+		return nil
+	})
 
-					if d < dateToRm.Day() {
-						return os.RemoveAll(pathCamYearMonthDay)
-					}
-					return nil
-				})
-				return err
-			})
-			return err
-		})
-		return err
+	return append(errs, errs2...)
+}
+
+func iterateMonths(path string) []error {
+	var errs []error
+	errs2 := utils.ForEachInDirectory(path, func(monthDir os.FileInfo) error {
+		monthInt, err := strconv.Atoi(monthDir.Name())
+		if err != nil {
+			return fmt.Errorf("error parsing month in \"%s\": %s", path, err)
+		}
+
+		if monthInt < int(earliestTimeToPreserve.Month()) {
+			return os.RemoveAll(filepath.Join(path, monthDir.Name()))
+		}
+
+		errs = append(errs, iterateDays(filepath.Join(path, monthDir.Name()))...)
+		return nil
+	})
+
+	return append(errs, errs2...)
+}
+
+func iterateDays(path string) []error {
+	return utils.ForEachInDirectory(path, func(dayDir os.FileInfo) error {
+		dayInt, err := strconv.Atoi(dayDir.Name())
+		if err != nil {
+			return fmt.Errorf("error parsing day in \"%s\": %s", path, err)
+		}
+
+		if dayInt < earliestTimeToPreserve.Day() {
+			return os.RemoveAll(filepath.Join(path, dayDir.Name()))
+		}
+
+		return nil
 	})
 }
