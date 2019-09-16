9714febdb1c6fe478df6e97d906c9daf80a9487d
Miguel-Dorta <contact@migueldorta.com>
2019-09-17 00:32:25 +0100

    Comment pkg/client and pkg/html packages

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -7,10 +7,14 @@ import (
 	"os"
 )
 
+// bufferSize is the size of the buffer for disk I/O.
+// The buffer itself will be defined in each call to preserve the thread safety.
 const bufferSize = 128 * 1024
 
+// HttpClient is the client that this package will use
 var HttpClient *http.Client
 
+// GetWithLogin makes a HTTP GET request using basic auth
 func GetWithLogin(url, user, pass string) (*http.Response, error) {
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
@@ -22,6 +26,7 @@ func GetWithLogin(url, user, pass string) (*http.Response, error) {
 	return HttpClient.Do(req)
 }
 
+// GetFileWithLogin downloads a file from URL to destination using basic auth
 func GetFileWithLogin(url, user, pass, destination string) error {
 	resp, err := GetWithLogin(url, user, pass)
 	if err != nil {
@@ -36,6 +41,9 @@ func GetFileWithLogin(url, user, pass, destination string) error {
 	defer f.Close()
 
 	if _, err = io.CopyBuffer(f, resp.Body, make([]byte, bufferSize)); err != nil {
+		// If failed, force close and try to remove it
+		_ = f.Close()
+		_ = os.Remove(destination)
 		return fmt.Errorf("error while saving file \"%s\": %s", destination, err)
 	}
 
--- pkg/html/a.go
+++ pkg/html/a.go
@@ -2,12 +2,15 @@ package html
 
 import "regexp"
 
+// A represents a simple <a> tag in HTML containing an HREF and the text itself
 type A struct {
 	HREF, Text string
 }
 
+// aRegex is the regex for parsing simple HTML <a> tags
 var aRegex = regexp.MustCompile("<a href=\"([0-9A-Za-z_./]+)\">([0-9A-Za-z_./]+)</a>")
 
+// GetAList gets a list of all the <a> tags in the data provided
 func GetAList(data []byte) []A {
 	matches := aRegex.FindAllSubmatch(data, -1)
 	result := make([]A, 0, len(matches))
